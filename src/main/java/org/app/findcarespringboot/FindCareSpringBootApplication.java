27f512846d17ac90d0abfbcff46636cf4b368cca
Prabhashperera <prabashmperera01@gmail.com>
2025-08-15 09:20:38 +0530

    project Init

---
+++ src/main/java/org/app/findcarespringboot/FindCareSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.app.findcarespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCareSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindCareSpringBootApplication.class, args);
+    }
+
+}
